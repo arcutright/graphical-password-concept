d6a4217cc7c157e74dae9af2d6b31b65e921f7f5
Aaron Cutright <ahc49@case.edu>
2016-04-08 16:14:11 -0400

    figured out how to communicate back and forth with server

--- changeImage.php
+++ changeImage.php
@@ -1,10 +1,31 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	if(isset($_POST['postData'])){
-		echo 'post data::: '.$_POST;
-		//$dataChain
-		//$clickPosition
+		$raw = json_decode($_POST['postData']);
+		// verify posted data is well-formed
+		if((array)$raw === $raw && sizeof($raw) == 3){
+			$dataChainIn = $raw[0];
+			$currentImageURL = $raw[1];
+			$chosenGridPosition = $raw[2];
+			// verify current image
+			if(url_exists($currentImageURL)){
+				// calculate next image, add to chain (+encrypt?)
+				$dataChainOut = '?';
+				$nextImageURL = '?';
+			}
+			else
+				header("HTTP/1.0 500 Internal Server Error");
+			// respond with [chain, next image url]
+			echo json_encode([$dataChainOut, $nextImageURL]);
+		}
+
 	}
-	else if(!empty($_POST)){
-		echo $_POST;
+
+	function url_exists($url){
+	   $headers = get_headers($url);
+	   return stripos($headers[0],"200 OK") ? true : false;
 	}
 ?>
